e9b09861d1dceb607f7d31c2d7ec640d8468d9c5
Trevor Brooks <trevorism@gmail.com>
2026-06-05 12:21:39 -0400

    Upgrade to the latest micronaut version, java version. Adding some more descriptions for failed tokens. Fixed 401vs403 issues

--- src/main/java/com/trevorism/AuthenticationFailedException.java
+++
@@ -1,8 +0,0 @@
-package com.trevorism;
-
-public class AuthenticationFailedException extends RuntimeException{
-    public AuthenticationFailedException(String message) {
-        super(message);
-    }
-}
-
